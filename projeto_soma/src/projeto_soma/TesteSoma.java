b5e5241cc18630ec20f75501e23465d6de7362db
Paulo Júnio de Souza Nascimento <paulobg8@gmail.com>
2021-03-30 13:21:42 -0300

    testando conhecimentos no git

--- projeto_soma/src/projeto_soma/TesteSoma.java
+++ projeto_soma/src/projeto_soma/TesteSoma.java
@@ -13,20 +13,20 @@ public class TesteSoma {
 //		int soma = n1 + n2;
 //		System.out.println("A soma dos valores é: " + soma);
 		
-//		Scanner teclado = new Scanner(System.in);
-//		System.out.print("digite seu ano de nascimento arrombado: ");
-//		int ano = teclado.nextInt();
-//		System.out.print("digite o ano atual arrombado: ");
-//		int anoAtual = teclado.nextInt();
-//		int calculo = anoAtual - ano;
-//		System.out.println("Sua idade é: " + calculo + "anos");{
-//			if(calculo >= 23) {
-//				System.out.println("você esta velho");
-//			} else {
-//				System.out.println("vc é novo!");
-//			}
-//			
-//		}
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("digite seu ano de nascimento arrombado: ");
+		int ano = teclado.nextInt();
+		System.out.print("digite o ano atual arrombado: ");
+		int anoAtual = teclado.nextInt();
+		int calculo = anoAtual - ano;
+		System.out.println("Sua idade é: " + calculo + "anos");{
+			if(calculo >= 23) {
+				System.out.println("você esta velho");
+			} else {
+				System.out.println("vc é novo!");
+			}
+			
+		}
 		
 		
 
