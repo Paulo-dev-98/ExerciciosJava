a44ba04523ac2cab519d1b8a1f4c48e3fbb1569a
Paulo Júnio de Souza Nascimento <paulobg8@gmail.com>
2021-03-31 12:22:28 -0300

    Trabalhando nos metodos do Bando de sangue.

--- projeto-banco-exercicio/src/exercicio_banco/BancoDeSangue.java
+++ projeto-banco-exercicio/src/exercicio_banco/BancoDeSangue.java
@@ -1,86 +1,115 @@
 package exercicio_banco;
 
 public class BancoDeSangue {
-	
+
 	private String nome;
 	private String cpf;
-	private String tipoDeSangue;
+	private String tipoDeSangueDoDoador;
+	private String tipoDeSangueDoReceptor;
 	private boolean status;
 	private String problemaDeSaude;
-	
+
 	public void estadoAtual() {
 		System.out.println("----------------------------------------------------------------------------------");
 		System.out.println("O nome do doador é: " + getNome());
-		System.out.println("O CPF do doador é: " +getCpf());
-		System.out.println("O tipo sanguinio do doador é:" + getTipoDeSangue());
-		System.out.println("O Status do cadastro é: " + getStatus() );
+		System.out.println("O CPF do doador é: " + getCpf());
+		System.out.println("O tipo sanguinio do doador é:" + getTipoDeSangueDoDoador());
+		System.out.println("O Status do cadastro é: " + getStatus());
 		System.out.println("O problema de saude do doador é: " + getProblemaDeSaude());
 	}
-	
-	
-	
+
+	public String getTipoDeSangueDoReceptor() {
+		return tipoDeSangueDoReceptor;
+	}
+
+	public void setTipoDeSangueDoReceptor(String tipoDeSangueDoReceptor) {
+		this.tipoDeSangueDoReceptor = tipoDeSangueDoReceptor;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-	public String getTipoDeSangue() {
-		return tipoDeSangue;
+
+	public String getTipoDeSangueDoDoador() {
+		return tipoDeSangueDoDoador;
 	}
-	public void setTipoDeSangue(String tipoDeSangue) {
-		this.tipoDeSangue = tipoDeSangue;
+
+	public void setTipoDeSangueDoDoador(String tipoDeSangue) {
+		this.tipoDeSangueDoDoador = tipoDeSangue;
 	}
+
 	public boolean getStatus() {
 		return status;
 	}
+
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
+
 	public String getProblemaDeSaude() {
 		return problemaDeSaude;
 	}
+
 	public void setProblemaDeSaude(String problemaDeSaude) {
 		this.problemaDeSaude = problemaDeSaude;
 	}
-	
+
 	public BancoDeSangue() {
 		this.setStatus(false);
 		this.setProblemaDeSaude("nenhum");
-		
+
 	}
-	
+
 	public void Cadastro(String nome, String cpd) {
 		this.setStatus(true);
 		this.setNome(nome);
 		this.setCpf(cpd);
-		
+
 	}
-	
+
 	public void doar(String tipoDeSangue, String problemaDeSaude) {
-		this.setTipoDeSangue(tipoDeSangue);
+		this.setTipoDeSangueDoDoador(tipoDeSangue);
 		this.setProblemaDeSaude(problemaDeSaude);
-		if(this.getStatus() == true) {
-			if(this.getProblemaDeSaude() == "diabetes") {
+		if (this.getStatus() == true) {
+			if (this.getProblemaDeSaude() == "diabetes") {
 				System.out.println("você não pode realizar a doação pois você tem: " + getProblemaDeSaude());
-			} else if(this.getProblemaDeSaude() == "hipertenção") {
+			} else if (this.getProblemaDeSaude() == "hipertenção") {
 				System.out.println("você não pode fazer a doação pois você tem: " + getProblemaDeSaude());
-			}else {
+			} else {
 				System.out.println("Doação autorizada, obrigado!");
 			}
-		}else {
+		} else {
 			System.out.println("você não pode doar por que não é cadastrado");
 		}
-		
+
 	}
 	
-	
+	public void receber(String tipoDeSangueDoReceptor, String tipoDeSangueDoDoador) {
+		this.setTipoDeSangueDoReceptor(tipoDeSangueDoReceptor);
+		this.setTipoDeSangueDoDoador(tipoDeSangueDoDoador);
+		if(this.getStatus() == true){
+			if(this.getTipoDeSangueDoReceptor() != this.getTipoDeSangueDoDoador()) {
+				System.out.println("doação incompativel");
+			}else {
+				System.out.println("doação realizada, obrigado!");
+			}
+			
+		}else {
+			System.out.println("imposivel receber doação pois você não está cadastrado");
+		}
+	}
 	
 
 }
--- projeto-banco-exercicio/src/exercicio_banco/TesteContaDeSangue.java
+++ projeto-banco-exercicio/src/exercicio_banco/TesteContaDeSangue.java
@@ -4,14 +4,19 @@ public class TesteContaDeSangue {
 
 	public static void main(String[] args) {
 		
-		BancoDeSangue contaAugusto = new BancoDeSangue();
-		contaAugusto.Cadastro("Augusto", "565.582.654.58");
-		contaAugusto.doar("AA", "nenhum");
-		contaAugusto.estadoAtual();
+//		BancoDeSangue contaAugusto = new BancoDeSangue();
+//		contaAugusto.Cadastro("Augusto", "565.582.654.58");
+//		contaAugusto.doar("AA", "nenhum");
+//		contaAugusto.estadoAtual();
+//		
+//		BancoDeSangue contaZick = new BancoDeSangue();
+//		contaZick.doar("B", "nenhum");
+//		contaZick.estadoAtual();
 		
-		BancoDeSangue contaZick = new BancoDeSangue();
-		contaZick.doar("B", "nenhum");
-		contaZick.estadoAtual();
+		BancoDeSangue contaTrevor = new BancoDeSangue();
+		contaTrevor.Cadastro("Trevor", "456.693.458.21");
+		contaTrevor.receber("B", "A");
+		
 
 	}
 
